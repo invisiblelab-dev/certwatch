ef8375970c39e698f739f7347130c9e05972bdaa
Vicente Munoz <vicentemunozlh@gmail.com>
2023-07-20 16:03:39 +0100

    certificate runners unified

--- internal/runners/certificates.go
+++ internal/runners/certificates.go
@@ -99,3 +99,23 @@ func CertArrayGetter(domainsArray []string) SSLInfoArray {
 
 	return sslInfoArray
 }
+
+type DomainDeadline struct {
+	Domain           string
+	DaysTillDeadline float64
+}
+
+type DomainsDeadlines struct {
+	Deadlines []DomainDeadline
+}
+
+func CalculateDeadline(certificates SSLInfoArray) DomainsDeadlines {
+	domainsDeadlines := DomainsDeadlines{}
+	for i := 0; i < len(certificates.DomainsSSLs); i++ {
+		timeHours := time.Until(certificates.DomainsSSLs[i].SSL.PeerCertificates[0].NotAfter)
+		timeDays := timeHours.Hours() / 24
+		deadline := DomainDeadline{Domain: certificates.DomainsSSLs[i].Domain, DaysTillDeadline: timeDays}
+		domainsDeadlines.Deadlines = append(domainsDeadlines.Deadlines, deadline)
+	}
+	return domainsDeadlines
+}
