bbaae67afca6c58710b6787f8dfba86b742d9adf
Vicente Munoz <vicentemunozlh@gmail.com>
2023-07-20 14:15:30 +0100

    removed main.go

--- internal/commands/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/invisiblelab-dev/certwatch/internal/runners"
-
-func main() {
-	domainsString := []string{"https://www.youtube.com/", "https://www.facebook.com/", "https://www.invisiblelab.dev/", "https://www.golangprograms.com/"}
-
-	runners.CreateSSLJSON(domainsString)
-}
